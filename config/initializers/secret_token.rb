37181a1a0096ae7e0192446bdbb0fd555ba62442
Matt Schmitter <matt.schmitter@gmail.com>
2015-10-18 02:03:04 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shareyoride::Application.config.secret_key_base = '99895d0e3257a76fa45678425266feefeeafb933ccfb97bc28eb38faca083629214036606e5fdbaed7cad9bb53adfa865a1199b30aa541f6bcbbf55c47477cf5'
